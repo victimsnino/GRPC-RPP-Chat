e8be8c53812e8ff2100b3233a3fc78e4bdf8c121
Aleksey Loginov <victimsnino@gmail.com>
2024-06-22 23:11:50 +0300

    use rppgrpc

--- src/client/chat_client/chat_client.cpp
+++ src/client/chat_client/chat_client.cpp
@@ -6,6 +6,7 @@
 #include <grpc++/support/sync_stream.h>
 
 #include <rpp/rpp.hpp>
+#include <rppgrpc/rppgrpc.hpp>
 #include <common.hpp>
 
 namespace ChatClient
@@ -31,103 +32,6 @@ namespace ChatClient
 
         private:
             grpc::string ticket_;
-        };
-
-        template<rpp::constraint::decayed_type Request, rpp::constraint::decayed_type Response>
-        class client_bidi_reactor final : public grpc::ClientBidiReactor<Request, Response>
-        {
-            using Base = grpc::ClientBidiReactor<Request, Response>;
-
-        public:
-            client_bidi_reactor()
-            {
-                m_requests.get_observable().subscribe(
-                    [this]<rpp::constraint::decayed_same_as<Request> T>(T&& message) {
-                        std::lock_guard lock{m_write_mutex};
-                        m_write.push_back(std::forward<T>(message));
-                        if (m_write.size() == 1)
-                            Base::StartWrite(&m_write.front());
-                    },
-                    [this](const std::exception_ptr&) {
-                        Base::StartWritesDone();
-                    },
-                    [this]() {
-                        Base::StartWritesDone();
-                    });
-            }
-
-            void init()
-            {
-                Base::StartCall();
-                Base::StartRead(&m_read);
-            }
-
-            auto get_observer()
-            {
-                return m_requests.get_observer();
-            }
-
-            auto get_observable()
-            {
-                return m_observer.get_observable();
-            }
-
-        private:
-            using Base::StartCall;
-            using Base::StartRead;
-
-            void OnReadDone(bool ok) override
-            {
-                if (!ok)
-                {
-                    m_observer.get_observer().on_error(std::make_exception_ptr(std::runtime_error{"OnReadDone is not ok"}));
-                    return;
-                }
-                m_observer.get_observer().on_next(m_read);
-                Base::StartRead(&m_read);
-            }
-
-            void OnWriteDone(bool ok) override
-            {
-                if (!ok)
-                {
-                    m_requests.get_disposable().dispose();
-                    m_observer.get_observer().on_error(std::make_exception_ptr(std::runtime_error{"OnWriteDone is not ok"}));
-                    return;
-                }
-
-                std::lock_guard lock{m_write_mutex};
-                m_write.pop_front();
-
-                if (!m_write.empty())
-                {
-                    Base::StartWrite(&m_write.front());
-                }
-            }
-
-            void OnDone(const grpc::Status& s) override
-            {
-                std::cout << "ON DONE " << s.error_code() << std::endl;
-                m_requests.get_disposable().dispose();
-                if (s.ok())
-                {
-                    m_observer.get_observer().on_completed();
-                }
-                else
-                {
-                    m_observer.get_observer().on_error(std::make_exception_ptr(std::runtime_error{s.error_message()}));
-                }
-                delete this;
-            }
-
-        private:
-            rpp::subjects::serialized_publish_subject<Request> m_requests{};
-
-            rpp::subjects::publish_subject<Response> m_observer;
-            Response                                 m_read{};
-
-            std::mutex          m_write_mutex{};
-            std::deque<Request> m_write{};
         };
     }
 
@@ -155,7 +59,7 @@ namespace ChatClient
 
             rpp::subjects::serialized_publish_subject<std::string>    messages{};
 
-            const auto reactor = new client_bidi_reactor<ChatService::Proto::Event::Message, ChatService::Proto::Event>();
+            const auto reactor = new rppgrpc::client_bidi_reactor<ChatService::Proto::Event::Message, ChatService::Proto::Event>();
             messages.get_observable()
                 | rpp::ops::map([](const std::string& txt) {
                       ChatService::Proto::Event::Message m{};
